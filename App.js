170d29a6d1f5a9bc539944c489343ecda0616eaf
jonathanmagal <80472970+jonathanmagal@users.noreply.github.com>
2021-05-23 11:58:42 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,61 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+import Axios from 'axios';
+
+function App() {
+
+  const [foodName, setFoodName] = useState('');
+  const [days, setDays] = useState(0);
+  const [newFoodName, setNewFoodName] = useState('')
+
+  const [foodList, setFoodList] = useState([]);
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/read").then((response) => {
+      setFoodList(response.data)
+    })
+  },[])
+
+  const addToList = () => {
+    Axios.post("http://localhost:3001/insert", {
+      foodName: foodName,
+      days: days,
+    });
+  };
+
+  const updateFood = (id) => [
+    Axios.put("http://localhost:3001/update",{id: id, newFoodName: newFoodName})
+  ]
+
+  const deleteFood = (id) => [
+    Axios.delete(`http://localhost:3001/delete/${id}`)
+  ]
+
+  return (
+    <div className="App">
+      <h1>Crud app</h1>
+      <label>Food Name:</label>
+      <input type="text" onChange={(event) => {setFoodName(event.target.value)}} />
+      <label>Days Since You Ate It</label>
+      <input type="number" onChange={(event) => {setDays(event.target.value)}} />
+      <button onClick={addToList}>Add To List</button>
+     
+      <hr />
+      <h1> Food List:</h1>
+      {foodList.map((val, key) => {
+        return (
+          <div>
+            <hr />
+            <h1>{val.foodName}</h1>
+            <h1>{val.daysSinceIAte}</h1>
+            <input type="text" placeholder="New Food Name..." onChange={(event) => {setNewFoodName(event.target.value)}} />
+            <button onClick={() => updateFood(val._id)}>Update</button>
+            <button onClick={() => deleteFood(val._id)}>Delete</button>
+          </div>
+        )
+      })}
+    </div>
+  );
+}
+
+export default App;
